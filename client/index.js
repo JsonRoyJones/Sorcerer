9a37abb869e2a0bc6176d3fc73d25fe26ed3a032
Jason Jones <jasonroyjones@gmail.com>
2020-01-02 14:29:53 -0800

    issues form has been completed, but route needs definition

--- client/index.js
+++ client/index.js
@@ -30,9 +30,10 @@ async function getRepos() {
   const url = 'https://api.github.com/search/repositories?q=stars:50000..300000';
   const response = await fetch(url);
   const result = await response.json();
-
   result.items.forEach(i => {
     console.log(i);
+    const div = document.createElement('div');
+    div.id = i.name;
     const img = document.createElement('img');
     img.src = i.owner.avatar_url;
     img.style.width = '32px';
@@ -40,45 +41,118 @@ async function getRepos() {
     const anchor = document.createElement('a');
     anchor.href = i.html_url;
     anchor.textContent = i.name;
-    divResult.appendChild(img);
-    divResult.appendChild(anchor);
-    divResult.appendChild(document.createElement('br'));
-    // appending getIssues and getCommits buttons to each i returned
-    divResult.appendChild(document.createElement('button'));
-    divResult.lastChild.setAttribute('id', `${i.name}_Issues`);
+    divResult.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(anchor);
+    div.appendChild(document.createElement('br'));
+    // appending getIssues buttons to each i returned
+    div.appendChild(document.createElement('button'));
+    div.lastChild.setAttribute('id', `${i.name}_Issues`);
+
     let curBtn;
     curBtn = document.getElementById(`${i.name}_Issues`);
+    console.log(curBtn);
     curBtn.textContent = `Get Issues`;
+    curBtn.addEventListener('click', () => {
+      console.log(i);
+      let brTag = document.createElement('br');
+      let btnParent = document.getElementById(i.name);
+      btnParent.appendChild(brTag);
+      getIssues(i.full_name);
+    });
     // getCommits button for each i
-    divResult.appendChild(document.createElement('button'));
-    divResult.lastChild.setAttribute('id', `${i.name}_Commits`);
-    curBtn = document.getElementById(`${i.name}_Commits`);
-    curBtn.textContent = `Get Commits`;
+    // divResult.appendChild(document.createElement('button'));
+    // divResult.lastChild.setAttribute('id', `${i.name}_Commits`);
+    // curBtn = document.getElementById(`${i.name}_Commits`);
+    // curBtn.textContent = `Get Commits`;
 
-    // curBtn.
-    // ('type', 'submit'),
-    // ('formmethod', 'get' + i.html_url),
-    // ('formtarget', '_blank')
     divResult.appendChild(document.createElement('br'));
   });
 }
 
-async function getIssues() {
-  clear();
+async function getIssues(repo) {
   // still need to add modular repo selection
-  const url = 'https://api.github.com/search/issues?q=repo:freecodecamp/freecodecamp type:issue';
+  const url = 'https://api.github.com/search/issues?q=repo:' + repo + ' type:issue state:open';
   const response = await fetch(url);
   const result = await response.json();
+  // select div for issues
+  const name = repo.split('/')[1];
+  const issues = document.getElementById(name);
+  if (result.items.length < 1) {
+    const noResults = document.createElement('p');
+    noResults.innerText =
+      "No issues available. It's likely that issues just aren't being shared by this repo.";
+    issues.appendChild(noResults);
+  } else {
+    {
+      /* <form action="//localhostwww.html.am/html-codes/forms/html-form-tag-action.cfm" target="result" method="get">
+<p>What would you like for lunch?</p>
+<input type="checkbox" name="issue" value="${el.title"> Apple
+<input type="checkbox" name="fruit" value="Orange"> Orange 
+<input type="checkbox" name="fruit" value="Banana"> Banana 
+<p><input type="submit" value="Save Issues"></p>
+</form>
+<h3>Result:</h3>
+<iframe name="result" style="height:100px;width:200px;"></iframe> */
+    }
+    // create form element for issues
+    const issuesForm = document.createElement('form');
+    issuesForm.action = console.log('URL GOES HERE');
+    issuesForm.target = 'result';
+    issuesForm.method = 'post';
 
-  result.items.forEach(i => {
-    console.log(i);
-    const anchor = document.createElement('a');
-    anchor.href = i.html_url;
-    anchor.textContent = i.title;
-    divResult.appendChild(anchor);
-    divResult.appendChild(document.createElement('br'));
-  });
+    // create submit button INPUT
+    const saveIssues = document.createElement('input');
+    saveIssues.type = 'submit';
+    saveIssues.value = `Save ${name} Issues`;
+
+    // append form element to issues div
+    issues.appendChild(issuesForm);
+
+    result.items.forEach(el => {
+      console.log('in result.items.forEach', el.user.avatar_url);
+      // checkbox and attributes
+      console.log(el);
+      const box = document.createElement('input');
+      box.type = 'checkbox';
+      box.name = el.title;
+      box.value = el.html_url;
+      // img icon and attributes
+      const img = document.createElement('img');
+      img.src = el.user.avatar_url;
+      img.style.width = '32px';
+      img.style.height = '32px';
+      // anchor element and attributes
+      const anchor = document.createElement('a');
+      anchor.href = el.html_url;
+      anchor.textContent = el.title;
+      // appending child nodes to issues element
+      issuesForm.appendChild(box);
+      issuesForm.appendChild(img);
+      issuesForm.appendChild(anchor);
+      issuesForm.appendChild(document.createElement('br'));
+    });
+    // append submit button to issuesForm
+    issuesForm.appendChild(saveIssues);
+  }
 }
+
+// async function getIssues() {
+//   clear();
+//   // still need to add modular repo selection
+//   const url = 'https://api.github.com/search/issues?q=repo:freecodecamp/freecodecamp type:issue';
+//   const response = await fetch(url);
+//   const result = await response.json();
+
+//   result.items.forEach(i => {
+//     console.log(i);
+//     const anchor = document.createElement('a');
+//     anchor.href = i.html_url;
+//     anchor.textContent = i.title;
+//     divResult.appendChild(anchor);
+//     divResult.appendChild(document.createElement('br'));
+//   });
+// }
 
 async function getIssuesPrivate() {
   clear();
